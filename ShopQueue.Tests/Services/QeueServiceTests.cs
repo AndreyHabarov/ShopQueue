c0722ece9be304a31244f91891f27690da5a0a24
Andrey Khabarov <andrewdronofff@gmail.com>
2026-04-01 21:28:45 +0300

    - add xUnit Tests

---
+++ ShopQueue.Tests/Services/QeueServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Moq;
+using ShopQueue.Application.Exceptions;
+using ShopQueue.Application.Repositories;
+using ShopQueue.Domain.Entities;
+using ShopQueue.Infrastructure.Services;
+using Xunit;
+
+namespace ShopQueue.Tests.Services;
+
+public class QueueServiceTests
+{
+    private readonly Mock<IQueueRepository> _queueRepositoryMock = new();
+    private readonly Mock<IQueueEntryRepository> _queueEntryRepositoryMock = new();
+    private readonly Mock<IShopRepository> _shopRepositoryMock = new();
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
+    private readonly Mock<IPublishEndpoint> _publishEndpointMock = new();
+    private readonly Mock<ILogger<QueueService>> _loggerMock = new();
+
+    private readonly QueueService _sut;
+
+    public QueueServiceTests()
+    {
+        _sut = new QueueService(
+            _shopRepositoryMock.Object,
+            _queueRepositoryMock.Object,
+            _queueEntryRepositoryMock.Object,
+            _unitOfWorkMock.Object,
+            _publishEndpointMock.Object,
+            _loggerMock.Object);
+    }
+
+    [Fact]
+    public async Task CallNextAsync_ShouldThrowNotFoundException_WhenQueueNotFound()
+    {
+        //Arrange
+        var queueId = Guid.NewGuid();
+
+        _queueRepositoryMock
+            .Setup(x => x.GetByIdAsync(queueId, default))
+            .ReturnsAsync((Queue?)null);
+
+        //Act
+        var act = async () => await _sut.CallNextAsync(queueId);
+
+        //Assert
+        await Assert.ThrowsAsync<NotFoundException>(act);
+    }
+}
